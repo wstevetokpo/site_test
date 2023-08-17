a6289da147806babc6582d46607a5489439e458a
Steve Tokpo <stevendstokpo60@gmail.com>
2023-08-17 16:44:24 +0100

    c1

--- content/index.php
+++ content/index.php
@@ -39,15 +39,95 @@
         ]
     ];
 
+    $services = [
+		[
+			'icon' => 'cube-outline',
+			'title' => 'Conception de produits',
+			'content' => 'Un coefficient large et efficace de génération et de développement</br>d\'idées à travers un processus qui conduit à de nouveaux produits.'
+		],
+		[
+			'icon' => 'layers-outline',
+			'title' => 'Création de marque',
+			'content' => 'À travers des concepts innovants, nous développons la stratégie et</br> l\'histoire de la marque afin de donner à l\'entreprise de l\'autorité.'
+		],
+		[
+			'icon' => 'videocam-outline',
+			'title' => 'Production vidéo',
+			'content' => 'Montage vidéo structuré pour présenter toutes les informations du projet,</br> y compris les films et les émissions de télévision, les publicités et la publicité.'
+		],
+		[
+			'icon' => 'bag-check-outline',
+			'title' => 'Marketing numérique',
+			'content' => 'Nous intégrons des canaux numériques tels que les moteurs de recherche,</br> les médias sociaux, les e-mails et d\'autres, afin d\'attirer des clients potentiels.'
+		],
+		[
+			'icon' => 'camera-outline',
+			'title' => 'Photographie artistique',
+			'content' => 'Que ce soit pour un événement décontracté ou professionnel, faire appel</br> à un professionnel fait toute la différence en matière de photographie.'
+		],
+		[
+			'icon' => 'game-controller-outline',
+			'title' => 'Développement de jeux',
+			'content' => 'L\'un des segments numériques les plus rentables au monde nécessite</br> une équipe créative et spécialisée pour concrétiser votre idée.'
+		]
+	];
+
 ?>
 <!DOCTYPE HTML>
 <html lang="<?= $requestLang ?>">
 <?php $page_title = 'Acceuil du site test'; $page_md = 'Découvrez notre petite start-up digital' ?>
 <?php include __DIR__ . DIRECTORY_SEPARATOR . 'part/head.php'; ?>
+<style>
+    .scrollable-container {
+        display: flex;
+        overflow-x: scroll;
+        overflow-y: hidden;
+    }
+
+    .scrollable-content {
+        white-space: nowrap;
+    }
+</style>
 <body class="shock-body">
     <?php include __DIR__ . DIRECTORY_SEPARATOR . 'part/header.php'; ?>
     <main id="main" class="shock-main bg-color black">
-        <section id="home" class="shock-section dynamic-slider is-banner scheme-2">
+        <section id="home" class="shock-section has-overlay" style="height: 100vh !important">
+            <div class="banner" style="height: 100vh !important">
+                <div class="content-wrapper text-center">
+                    <div class="basic-intro mb-35">
+                        <h2 class="title text-style-5">
+                            <span class="text-1 text-outline white-75">
+                                Un endroit pur
+                            </span>
+                            <span class="text-2 text-italic white">
+                                pour trouver <mark class="animated-underline tertiary"> les meilleurs services</mark>.
+                            </span>
+                        </h2>
+                        <div class="description white-50">
+                            <p>
+                                Notre mission est de fournir à nos clients des produits qui transformeront la manière dont les gens voient le monde.<br>
+                                Transformer une idée en quelque chose qui fonctionne, voilà ce qui nous anime.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="gallery mt-3">
+                        <a href="<?= HOME_PATH ?>assets/img/c-22.png" class="button outline lightbox-link white tertiary-hover magnetic-effect">
+                            <span class="button-text white white-hover">
+                                En savoir plus
+                            </span>
+                            <i class="fa-solid fa-arrow-right button-icon white white-hover"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="image-wrapper" style="height: 100vh !important">
+                    <video class="video vh-75 fit-cover" style="height: 100vh !important" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+                        <source src="<?= HOME_PATH ?>assets/vid/b-29.mp4" type="video/mp4">
+                    </video>
+                </div>
+                <div class="overlay black-50"></div>
+            </div>
+        </section>
+        <section class="shock-section dynamic-slider is-banner scheme-2">
             <div id="slide-index" class="slide-index">
                 SERVICES D'EXCELLENCE
             </div>
@@ -252,44 +332,48 @@
                 </div>
             </div>
         </section>
-        
-        <section class="shock-section has-overlay">
-            <div class="banner">
-                <div class="content-wrapper text-center">
-                    <div class="basic-intro mb-35">
-                        <h2 class="title text-style-5">
-                            <span class="text-1 text-outline white-75">
-                                Un endroit pur
-                            </span>
-                            <span class="text-2 text-italic white">
-                                pour respirer<mark class="animated-underline tertiary">de bonnes choses</mark>.
-                            </span>
-                        </h2>
-                        <div class="description white-50">
-                            <p>
-                                Notre mission est de fournir à nos clients des produits qui transformeront la manière dont les gens voient le monde.<br>
-                                Transformer une idée en quelque chose qui fonctionne, voilà ce qui nous anime.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="gallery">
-                        <a href="<?= HOME_PATH ?>assets/img/c-22.png" class="button outline lightbox-link white tertiary-hover magnetic-effect">
-                            <span class="button-text white white-hover">
-                                En savoir plus
-                            </span>
-                            <i class="fa-solid fa-arrow-right button-icon white white-hover"></i>
-                        </a>
-                    </div>
+
+        <section class="shock-section pt-5 pb-4 bg-color black">
+            <div class="sticky-container">
+                <div class="scrollable-container">
+					<?php
+						foreach ($services as $key => $value) {
+					?>
+							<div class="scrollable-content" data-aos="fade-up" data-aos-delay="600">
+								<div class="card double-edge has-icon parent" style="height: 350px;ajustify-content: center;">
+									<div class="card-body">
+										<img class="card-image-icon primary" src="<?= HOME_PATH . 'assets/img/' . $value['icon'] . '.svg' ?>" alt="Icon"  data-shock-icon="32" />
+										<h3 class="title text-style-11 white">
+											<?= $value['title'] ?>
+										</h3>
+										<p class="description">
+											<?= $value['content'] ?>
+										</p>
+										<div class="button-wrapper align-h-right">
+											<span class="arrow-button cross scheme-2 primary">
+												<span class="arrow">
+													<span class="item"></span>
+													<span class="item"></span>
+												</span>
+												<span class="line"></span>
+												<span class="text">
+													VOIR PLUS
+												</span>
+											</span>
+										</div>
+									</div>
+									<a href="<?= HOME_PATH ?>nous-contacter" class="full-link">
+										<div class="overlay gray-25 magnetic-effect"></div>
+									</a>
+								</div>
+							</div>
+					<?php
+						}
+					?>
                 </div>
-                <div class="image-wrapper">
-                    <video class="video vh-75 fit-cover" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-                        <source src="<?= HOME_PATH ?>assets/vid/b-29.mp4" type="video/mp4">
-                    </video>
-                </div>
-                <div class="overlay black-50"></div>
             </div>
         </section>
-        
+
         <section class="shock-section pt-6 pb-6 bg-color black js-switch-price">
             <div class="container">
                 <div class="basic-intro mb-35 text-center">
@@ -645,5 +729,17 @@
     </main>
     <?php include __DIR__ . DIRECTORY_SEPARATOR . 'part/footer.php'; ?>
     <?php include __DIR__ . DIRECTORY_SEPARATOR . 'part/script.php'; ?>
+    <script>
+        const element = document.querySelector(".scrollable-container");
+
+        element.addEventListener('wheel', (event) => {
+            event.preventDefault();
+
+            element.scrollBy({
+                left: event.deltaY < 0 ? -30 : 30,
+                
+            });
+        });
+    </script>
 </body>
 </html>
